f38a23888031ce59fb319b2d9263b45be88668f3
sabr <sabbraxcaddabra@yandex.ru>
2023-02-06 20:50:39 +0300

    Добавил графики в первом приближении

--- stats_page.py
+++ stats_page.py
@@ -1,6 +1,7 @@
 from dash import dcc, html, dash_table
 from dash import Input, Output, State, callback
 import json
+import plotly.express as px
 
 import dash_bootstrap_components as dbc
 
@@ -14,6 +15,67 @@ with open('./data/stat_years.json', encoding='utf8') as stats:
 
 with open('./data/to_rus.json', encoding='utf8') as localize_fp:
     LOCALIZE_DICT = json.load(localize_fp)
+
+def get_df_by_base_names(base_names, df):
+    comparation_list = {
+         'count_human': 'Количество человек, подавших заявление',
+         'count_apps': 'Количество заявлений',
+         'count_apps_obsh': 'Количество заявлений, поданных на общих основаниях',
+         'sred_obsh': 'Средний балл абитуриентов, подавших заявление на общих основаниях',
+         'count_apps_cel': 'Количество заявлений, поданных на целевую квоту',
+         'sred_cel': 'Средний балл абитуриентов, подавших заявление на целевую квоту',
+         'count_apps_kvot': 'Количество заявлений, поданных на особую и специальную квоту',
+         'sred_kvot': 'Средний балл абитуриентов, подавших заявление на особую и специальную квоту',
+         'count_apps_plat': 'Количество заявлений, поданных на места по договорам об оказании платных услуг',
+         'sred_plat': 'Средний балл абитуриентов, подавших заявление на места по договорам об оказании платных услуг',
+         'count_apps_obsh_post': 'Количество заявлений, по которым абитуриенты были зачислены на общих основаниях',
+         'sred_obsh_post': 'Средний балл зачисленных абитуриентов на общих основаниях',
+         'min_obsh_post': 'Минимальный проходной балл зачисленного абитуриента на общих основаниях',
+         'count_apps_cel_post': 'Количество заявлений, по которым абитуриенты были зачислены на целевую квоту',
+         'sred_cel_post': 'Средний балл зачисленных абитуриентов на целевую квоту',
+         'min_cel_post': 'Минимальный проходной балл зачисленного абитуриента на целевую квоту',
+         'count_apps_kvot_post': 'Количество заявлений, по которым абитуриенты были зачислены на особую и специальную квоту',
+         'sred_kvot_post': 'Средний балл зачисленных абитуриентов на особую и специальную квоту',
+         'min_kvot_post': 'Минимальный проходной балл зачисленного абитуриента на особую и специальную квоту',
+         'count_apps_plat_post': 'Количество заявлений, по которым абитуриенты были зачислены на места по договорам об оказании платных услуг',
+         'sred_plat_post': 'Средний балл зачисленных абитуриентов на места по договорам об оказании платных услуг',
+         'min_plat_post': 'Минимальный проходной балл зачисленного абитуриента на места по договорам об оказании платных услуг'}
+    indexes = [list(comparation_list).index(x) for x in base_names]
+    titles = [comparation_list[x] for x in base_names]
+    d = [dict(df.iloc[index][1:]) for index in indexes]
+    years = list(d[0])
+    counts = [list(x.values()) for x in d]
+    dict_to_df = {titles[x]: counts[x] for x in range(len(base_names))}
+    dict_to_df['Год'] = years
+    df = pd.DataFrame(dict_to_df)
+    return df
+
+def get_fig(data_frame, title):
+
+    fig = px.bar(data_frame, x=data_frame['Год'], y=data_frame.columns, barmode='group',
+                 height=400)
+    fig.update_layout(
+        xaxis_title="Год",
+        yaxis_title=title,
+        legend_title="",
+        legend=dict(
+            x=0,
+            y=1,
+            traceorder="reversed",
+            title_font_family="Times New Roman",
+            font=dict(
+                family="Courier",
+                size=12,
+                color="black"
+            ),
+            bgcolor="White",
+            bordercolor="Black",
+            borderwidth=2,
+            yanchor="bottom"
+        )
+    )
+
+    return fig
 
 DEFAULT_DICT = {
     "code": "-",
@@ -76,10 +138,39 @@ def generate_layout(kaf_name):
         dbc.Col(
             html.Div(children='', id='kaf_stats')
         ),
+        dcc.Graph(id='fig1', style={'height': '80vh'}),
+        dcc.Graph(id='fig2', style={'height': '80vh'}),
+        dcc.Graph(id='fig3', style={'height': '80vh'}),
+        dcc.Graph(id='fig4', style={'height': '80vh'}),
+        dcc.Graph(id='fig5', style={'height': '80vh'}),
     ])
 
     return stats
 
+@callback(
+    [Output('fig1', 'figure'), Output('fig2', 'figure'), Output('fig3', 'figure'), Output('fig4', 'figure'), Output('fig5', 'figure')],
+    [Input('spec_codes', 'value')]
+)
+def get_plots(spec_code):
+    df = get_stats_df(get_spec_code_stats(spec_code))
+
+    df_1 = get_df_by_base_names(['sred_obsh', 'sred_cel', 'sred_kvot', 'sred_plat'], df)
+
+    df_2 = get_df_by_base_names(['count_apps_obsh', 'count_apps_plat'], df)
+
+    df_3 = get_df_by_base_names(['count_apps_cel', 'count_apps_kvot'], df)
+
+    df_4 = get_df_by_base_names(['count_human'], df)
+
+    df_5 = get_df_by_base_names(['count_apps'], df)
+
+    fig_1 = get_fig(df_1, 'Средний балл абитуриентов')
+    fig_2 = get_fig(df_2, 'Количество заявлений (общий конкурс и ДОУ)')
+    fig_3 = get_fig(df_3, 'Количество заявлений (целевая и особая/специальная квота)')
+    fig_4 = get_fig(df_4, 'Количество человек, подавших заявление')
+    fig_5 = get_fig(df_5, 'Количество поданных заявлений')
+
+    return fig_5, fig_4, fig_1, fig_2, fig_3
 
 @callback(
     Output('kaf_stats', 'children'),
